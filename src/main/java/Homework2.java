ae8c84f30f773cc2a65decae1fcffdac69671fa6
Tillerg <mrgish@yandex.ru>
2022-07-28 23:30:57 +0300

    Homework1

--- src/main/java/Homework2.java
+++ src/main/java/Homework2.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Homework2 {
 
     public static void main(String[] args) {
-        int value;
+
 
         Settings workbox = new Settings(5,100,"Candy-land","Sweet Bob");
         workbox.takeCount();
