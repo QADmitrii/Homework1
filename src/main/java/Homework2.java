6f34d96c0aac261438f3dee643475669ddf692de
Tillerg <mrgish@yandex.ru>
2022-07-29 11:38:19 +0300

    comment

--- src/main/java/Homework2.java
+++ src/main/java/Homework2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+ //Вендинговый автомат
 public class Homework2 {
 
     public static void main(String[] args) {
@@ -7,6 +7,8 @@ public class Homework2 {
 
         Settings workbox = new Settings(5,100,"Candy-land","Sweet Bob");
         workbox.takeCount();
+
+
 
     }
 
